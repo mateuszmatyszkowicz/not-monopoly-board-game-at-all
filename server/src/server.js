7132e87159b03785a40b812dfc46ba9ef3815402
Mateusz Matyszkkowicz <mateusz.matyszkowicz@boldare.com>
2020-11-07 16:22:38 +0100

    nodemon

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 const webSocketsServerPort = 8000;
 const webSocketServer = require('websocket').server;
 const http = require('http');
+const { addListener } = require('process');
 const { connection } = require('websocket');
 // Spinning the http server and the websocket server.
 const server = http.createServer();
@@ -18,6 +19,8 @@ const getUniqueID = () => {
   const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
   return s4() + s4() + '-' + s4();
 };
+
+
 
 
 wsServer.on('request', function(request) {
