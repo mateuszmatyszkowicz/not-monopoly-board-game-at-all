2789ae7677f48b8402258d6564257b654da0e0ba
Adam S <azslomian@gmail.com>
2020-11-12 19:10:02 +0100

    added tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 import { eventType } from "./eventTypes";
+import './assets/tailwind.output.css';
 
 const client = new W3CWebSocket("ws://127.0.0.1:8000");
 
